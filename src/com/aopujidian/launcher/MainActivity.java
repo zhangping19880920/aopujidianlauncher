10ecc35993f5a908f57fdae0cf71b5bacfd439b1
zhangping <zhangping19880920@gmail.com>
2014-08-26 23:33:25 +0800

    add layout

--- src/com/aopujidian/launcher/MainActivity.java
+++ src/com/aopujidian/launcher/MainActivity.java
@@ -2,19 +2,46 @@ package com.aopujidian.launcher;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
-    @Override
+    private static final String TAG = "MainActivity";
+
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+    public void onClick (View view) {
+    	switch (view.getId()) {
+		case R.id.ib_top_first:
+			
+			break;
+		case R.id.ib_top_second:
+			
+			break;
+		case R.id.ib_top_third:
+			
+			break;
+		case R.id.ib_top_fourth:
+			
+			break;
+		case R.id.ib_bottom_first:
+			
+			break;
+		case R.id.ib_bottom_second:
+			
+			break;
+		case R.id.ib_bottom_third:
+			
+			break;
+		case R.id.ib_bottom_fourth:
+			
+			break;
+		}
     }
 }
