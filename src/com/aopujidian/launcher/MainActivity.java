35a3f9adbd9f9be36e7a9caa0f9cf17d93ab67da
zhangping <zhangping19880920@gmail.com>
2014-08-27 10:15:40 +0800

    demo ok

--- src/com/aopujidian/launcher/MainActivity.java
+++ src/com/aopujidian/launcher/MainActivity.java
@@ -2,13 +2,18 @@ package com.aopujidian.launcher;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     private static final String TAG = "MainActivity";
+    
+    private static final String ANDROID_LAUNCHER_PACKAGE = "com.android.launcher";
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,29 +24,59 @@ public class MainActivity extends Activity {
     public void onClick (View view) {
     	switch (view.getId()) {
 		case R.id.ib_top_first:
-			
+			Toast.makeText(getApplicationContext(), "点击了: " + getString(R.string.top_first), Toast.LENGTH_SHORT).show();
 			break;
 		case R.id.ib_top_second:
-			
+			Toast.makeText(getApplicationContext(), "点击了: " + getString(R.string.top_second), Toast.LENGTH_SHORT).show();
 			break;
 		case R.id.ib_top_third:
-			
+			Toast.makeText(getApplicationContext(), "点击了: " + getString(R.string.top_third), Toast.LENGTH_SHORT).show();
+			//TODO 进入电子屏幕
+			goActivity(Slide.class);
 			break;
 		case R.id.ib_top_fourth:
-			
+			Toast.makeText(getApplicationContext(), "点击了: " + getString(R.string.top_fourth) + "进入android 桌面", Toast.LENGTH_SHORT).show();
+			//TODO 进入android桌面
+			goLauncher();
 			break;
 		case R.id.ib_bottom_first:
-			
+			Toast.makeText(getApplicationContext(), "点击了: " + getString(R.string.buttom_first), Toast.LENGTH_SHORT).show();
 			break;
 		case R.id.ib_bottom_second:
-			
+			Toast.makeText(getApplicationContext(), "点击了: " + getString(R.string.buttom_second), Toast.LENGTH_SHORT).show();
+			//TODO 提示停车
+			goActivity(Parking.class);
 			break;
 		case R.id.ib_bottom_third:
-			
+			Toast.makeText(getApplicationContext(), "点击了: " + getString(R.string.buttom_third), Toast.LENGTH_SHORT).show();
 			break;
 		case R.id.ib_bottom_fourth:
-			
+			Toast.makeText(getApplicationContext(), "点击了: " + getString(R.string.buttom_fourth), Toast.LENGTH_SHORT).show();
 			break;
+		}
+    }
+    
+    /** 启动activity */
+    private void goActivity(Class<?> cls){
+    	Intent intent = new Intent(getApplicationContext(), cls);
+    	try {
+    		startActivity(intent);
+		} catch (ActivityNotFoundException e) {
+			Toast.makeText(getApplicationContext(), R.string.do_not_start_launcher, 0).show();
+		}
+    }
+    
+    /** 启动android桌面 */
+    private void goLauncher(){
+    	Intent intent = new Intent();
+		intent.setPackage(ANDROID_LAUNCHER_PACKAGE);
+		intent.setAction(Intent.ACTION_MAIN);
+		intent.addCategory(Intent.CATEGORY_HOME);
+		try {
+			startActivity(intent);
+		} catch (ActivityNotFoundException e) {
+			String msg = getString(R.string.do_not_start_launcher) + getString(R.string.top_fourth);
+			Toast.makeText(getApplicationContext(), msg, 0).show();
 		}
     }
 }
---
+++ src/com/aopujidian/launcher/Slide.java
@@ -0,0 +1,17 @@
+package com.aopujidian.launcher;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Slide extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.slide_activity);
+	}
+	
+	public void exit(View view) {
+		finish();
+	}
+}
