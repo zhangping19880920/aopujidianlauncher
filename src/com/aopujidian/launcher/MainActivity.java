358d691359f6c65f0af7e542f194e3028d9f1b6f
zhangping <zhangping19880920@gmail.com>
2014-08-31 11:55:58 +0800

    develop parking

--- src/com/aopujidian/launcher/MainActivity.java
+++ src/com/aopujidian/launcher/MainActivity.java
@@ -1,12 +1,34 @@
 package com.aopujidian.launcher;
 
+import java.io.BufferedInputStream;
+import java.io.Closeable;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore.Images;
+import android.provider.MediaStore.MediaColumns;
+import android.provider.MediaStore.Video;
+import android.provider.MediaStore.Images.ImageColumns;
+import android.provider.MediaStore.Video.VideoColumns;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
+import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
-import android.view.Menu;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -15,10 +37,164 @@ public class MainActivity extends Activity {
     
     private static final String ANDROID_LAUNCHER_PACKAGE = "com.android.launcher";
 
+    public static final String DCIM = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString();
+    public static final String DIRECTORY = DCIM + "/Camera";
+
+    // Match the code in MediaProvider.computeBucketValues().
+    public static final String BUCKET_ID = String.valueOf(DIRECTORY.toLowerCase().hashCode());
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        WindowManager windowManager = getWindowManager();  
+        Display display = windowManager.getDefaultDisplay();  
+        int screenWidth = screenWidth = display.getWidth();  
+        int screenHeight = screenHeight = display.getHeight();  
+        
+        Log.e(TAG, "screenWidth = " + screenWidth + " ,screenHeight = " + screenHeight);
+    }
+	
+    private static class Media {
+        public Media(long id, int orientation, long dateTaken, Uri uri) {
+            this.id = id;
+            this.orientation = orientation;
+            this.dateTaken = dateTaken;
+            this.uri = uri;
+        }
+
+        public final long id;
+        public final int orientation;
+        public final long dateTaken;
+        public final Uri uri;
+    }
+
+    public static Media getLastImageThumbnail(ContentResolver resolver) {
+        Uri baseUri = Images.Media.EXTERNAL_CONTENT_URI;
+
+        Uri query = baseUri.buildUpon().appendQueryParameter("limit", "1").build();
+        String[] projection = new String[] {ImageColumns._ID, ImageColumns.ORIENTATION,
+                ImageColumns.DATE_TAKEN};
+        String selection = ImageColumns.MIME_TYPE + "='image/jpeg' AND " +
+                ImageColumns.BUCKET_ID + '=' + BUCKET_ID;
+        String order = ImageColumns.DATE_TAKEN + " DESC," + ImageColumns._ID + " DESC";
+
+        Cursor cursor = null;
+        try {
+            cursor = resolver.query(query, projection, selection, null, order);
+            if (cursor != null && cursor.moveToFirst()) {
+                long id = cursor.getLong(0);
+                return new Media(id, cursor.getInt(1), cursor.getLong(2),
+                        ContentUris.withAppendedId(baseUri, id));
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+        return null;
+    }
+
+    private static Media getLastVideoThumbnail(ContentResolver resolver) {
+        Uri baseUri = Video.Media.EXTERNAL_CONTENT_URI;
+
+        Uri query = baseUri.buildUpon().appendQueryParameter("limit", "1").build();
+        String[] projection = new String[] {VideoColumns._ID, MediaColumns.DATA,
+                VideoColumns.DATE_TAKEN};
+        String selection = VideoColumns.BUCKET_ID + '=' + BUCKET_ID;
+        String order = VideoColumns.DATE_TAKEN + " DESC," + VideoColumns._ID + " DESC";
+
+        Cursor cursor = null;
+        try {
+            cursor = resolver.query(query, projection, selection, null, order);
+            if (cursor != null && cursor.moveToFirst()) {
+                Log.d(TAG, "getLastVideoThumbnail: " + cursor.getString(1));
+                long id = cursor.getLong(0);
+                return new Media(id, 0, cursor.getLong(2),
+                        ContentUris.withAppendedId(baseUri, id));
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+        return null;
+    }
+	
+    public static void closeSilently(Closeable c) {
+        if (c == null) return;
+        try {
+            c.close();
+        } catch (Throwable t) {
+            // do nothing
+        }
+    }
+    
+    public static boolean isUriValid(Uri uri, ContentResolver resolver) {
+        if (uri == null) return false;
+
+        try {
+            ParcelFileDescriptor pfd = resolver.openFileDescriptor(uri, "r");
+            if (pfd == null) {
+                Log.e(TAG, "Fail to open URI. URI=" + uri);
+                return false;
+            }
+            pfd.close();
+        } catch (IOException ex) {
+            return false;
+        }
+        return true;
+    }
+    
+    public static Uri getLastThumbnail(ContentResolver resolver) {
+        Media image = getLastImageThumbnail(resolver);
+        Media video = getLastVideoThumbnail(resolver);
+        if (image == null && video == null) return null;
+
+        Bitmap bitmap = null;
+        Media lastMedia;
+        // If there is only image or video, get its thumbnail. If both exist,
+        // get the thumbnail of the one that is newer.
+        if (image != null && (video == null || image.dateTaken >= video.dateTaken)) {
+            bitmap = Images.Thumbnails.getThumbnail(resolver, image.id,
+                    Images.Thumbnails.MINI_KIND, null);
+            lastMedia = image;
+        } else {
+            bitmap = Video.Thumbnails.getThumbnail(resolver, video.id,
+                    Video.Thumbnails.MINI_KIND, null);
+            lastMedia = video;
+        }
+
+        // Ensure database and storage are in sync.
+        if (isUriValid(lastMedia.uri, resolver)) {
+        	Log.e(TAG, "lastMedia.uri = " + lastMedia.uri);
+        	return lastMedia.uri;
+        }
+        return null;
+    }
+	
+    public static void loadFrom(File file) {
+    	Log.e(TAG, "loadFrom = " + file.getAbsolutePath());
+        Uri uri = null;
+        Bitmap bitmap = null;
+        FileInputStream f = null;
+        BufferedInputStream b = null;
+        DataInputStream d = null;
+            try {
+                f = new FileInputStream(file);
+                b = new BufferedInputStream(f, 4096);
+                d = new DataInputStream(b);
+                uri = Uri.parse(d.readUTF());
+                Log.e(TAG, "uri = " + uri.toString());
+                bitmap = BitmapFactory.decodeStream(d);
+                d.close();
+            } catch (IOException e) {
+                Log.i(TAG, "Fail to load bitmap. " + e);
+            } finally {
+                closeSilently(f);
+                closeSilently(b);
+                closeSilently(d);
+            }
     }
 
     public void onClick (View view) {
@@ -32,7 +208,11 @@ public class MainActivity extends Activity {
 		case R.id.ib_top_third:
 			Toast.makeText(getApplicationContext(), "点击了: " + getString(R.string.top_third), Toast.LENGTH_SHORT).show();
 			//TODO 进入电子屏幕
-			goActivity(Slide.class);
+//			goActivity(Slide.class);
+//			loadFrom(new File(getFilesDir(), "last_thumb"));
+			Uri lastThumbnail = getLastThumbnail(getContentResolver());
+			startActivity(new Intent("com.android.camera.action.REVIEW", lastThumbnail));
+//	        startActivity(intent);
 			break;
 		case R.id.ib_top_fourth:
 			Toast.makeText(getApplicationContext(), "点击了: " + getString(R.string.top_fourth) + "进入android 桌面", Toast.LENGTH_SHORT).show();
--- src/com/aopujidian/launcher/Parking.java
+++ src/com/aopujidian/launcher/Parking.java
@@ -1,17 +1,104 @@
 package com.aopujidian.launcher;
 
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
+import com.lidroid.xutils.view.annotation.event.OnClick;
+
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences.Editor;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Parking extends Activity {
+	
+	private static final String TAG = "Parking";
+	
+	private static final String PREFS_NAME = "config";
+	
+	private static final String PREFS_KEY_PARKING = "parking_text";
+	
+	@ViewInject(R.id.iv_background)
+	private ImageView mBackgroundImageView;
+	
+	@ViewInject(R.id.et_text)
+	private EditText mEdittext;
+	
+	@ViewInject(R.id.tv_text)
+	private TextView mTextView;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.slide_activity);
+		setContentView(R.layout.parking_activity);
+		ViewUtils.inject(this);
+		String latText = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE).getString(PREFS_KEY_PARKING, "");
+		if (null != latText && !TextUtils.isEmpty(latText)) {
+			mTextView.setText(latText);
+		}
 	}
-	
+
+	@OnClick(R.id.btn_exit)
 	public void exit(View view) {
 		finish();
+	}
+
+	@OnClick(R.id.btn_set_background)
+	public void setBackground(View view) {
+		Log.e(TAG, "setBackground");
+
+		Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+		intent.addCategory(Intent.CATEGORY_OPENABLE);
+		intent.setType("image/*");
+		intent.putExtra("crop", "true");
+		intent.putExtra("aspectX", 1.5);
+		intent.putExtra("aspectY", 1);
+		intent.putExtra("outputX", 300);
+		intent.putExtra("outputY", 300);
+		intent.putExtra("return-data", true);
+
+		startActivityForResult(intent, 0);
+	}
+	
+	@OnClick(R.id.btn_edit)
+	public void edit(View view){
+		if (mEdittext.isEnabled()) {
+			mEdittext.setEnabled(false);
+			mEdittext.setVisibility(View.GONE);
+			mTextView.setVisibility(View.VISIBLE);
+			String text = mEdittext.getText().toString();
+			mTextView.setText(text);
+			Editor edit = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE).edit();
+			edit.putString(PREFS_KEY_PARKING, mTextView.getText().toString());
+			edit.commit();
+		} else {
+			mEdittext.setEnabled(true);
+			mEdittext.setVisibility(View.VISIBLE);
+			mEdittext.requestFocus();
+			mEdittext.setText(mTextView.getText());
+			InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+			imm.toggleSoftInput(0, InputMethodManager.SHOW_FORCED);
+			mTextView.setVisibility(View.GONE);
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (null != data) {
+			Bitmap cameraBitmap = (Bitmap) data.getExtras().get("data");
+			if (null != cameraBitmap) {
+				mBackgroundImageView.setImageBitmap(cameraBitmap);
+			}
+		}
+		super.onActivityResult(requestCode, resultCode, data);
 	}
 }
